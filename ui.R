8e3c5484e54ca06b8d471da2a46d360588e619ec
bdavs <rdavis2@umassd.edu>
2015-07-22 14:29:54 -0400

    update merge

--- ui.R
+++ ui.R
@@ -7,7 +7,6 @@ shinyUI(fluidPage(
     sidebarPanel(
       tabsetPanel(
         tabPanel("subsystem 1",
-<<<<<<< HEAD
                  numericInput('Ttime1', 'Input Total time:', 20000, min = 0, step = 1000),
                  numericInput('Amode_fail_rate1', 'Input A mode Failure Rate:', 1000, step = 100),
                  numericInput('Bmode_fail_rate1', 'Input B mode Failure Rate:', 100, step = 50),
@@ -32,31 +31,6 @@ shinyUI(fluidPage(
       
     ),
      
-=======
-          numericInput('Ttime1', 'Input Total time:', 20000, min = 0, step = 1000),
-          numericInput('Amode_fail_rate1', 'Input A mode Failure Rate:', 1000, step = 100),
-          numericInput('Bmode_fail_rate1', 'Input B mode Failure Rate:', 100, step = 50),
-          numericInput('Bmode_FEF1', 'Input B mode Fix effectiveness factor:', 0.9, step = 0.1,min = 0, max = 1),
-          numericInput('Cost_Increment1', 'cost increment:', 5000000, step = 1000000),
-          sliderInput('Reliability_Investment_input1', 'Input Reliability Investment :', min = 0, max = 60000000,value = 20000000, step = 1000000),
-          numericInput('C01', 'Operating cost:', 1000000, step = 1000000),
-          numericInput('Ci1', 'Initial cost:', 200000, step = 10000),
-          width=3),
-      tabPanel("subsystem 2",
-               numericInput('Ttime2', 'Input Total time:', 20000, min = 0, step = 1000),
-               numericInput('Amode_fail_rate2', 'Input A mode Failure Rate:', 500, step = 100),
-               numericInput('Bmode_fail_rate2', 'Input B mode Failure Rate:', 200, step = 50),
-               numericInput('Bmode_FEF2', 'Input B mode Fix effectiveness factor:', 0.8, step = 0.1,min = 0, max = 1),
-               numericInput('Cost_Increment2', 'cost increment:', 4000000, step = 1000000),
-               sliderInput('Reliability_Investment_input2', 'Input Reliability Investment :', min = 0, max = 60000000,value = 20000000, step = 1000000),
-               numericInput('C02', 'Operating cost:', 800000, step = 1000000),
-               numericInput('Ci2', 'Initial cost:', 75000, step = 10000),
-               width=3),
-      id = "panelName")
-      
-      ),
-    
->>>>>>> b55d5f4038b260fb12b52a13a4499ac4756f9d9f
     # Show a plot of the generated distribution
     mainPanel(tabsetPanel(
       
@@ -64,7 +38,7 @@ shinyUI(fluidPage(
       tabPanel("Replacement Units", plotOutput("repParts",height = "700px")),
       tabPanel("Cost", plotOutput("Cost",height = "700px")),
       tabPanel("Fleet Size", plotOutput("fleetSize",height = "700px")),
-<<<<<<< HEAD
+
      # tabPanel("Max Fleet Size", plotOutput("maxFleet",height = "700px")),
       tabPanel("Affordability", plotOutput("Aff",height = "700px"))
      # tabPanel("Text", textOutput("MTTF_Output"))
@@ -73,14 +47,5 @@ shinyUI(fluidPage(
     )
     
   ))
-=======
-      #tabPanel("Max Fleet Size", plotOutput("maxFleet",height = "700px")),
-      tabPanel("Affordability", plotOutput("Aff",height = "700px")),
-      tabPanel("Text", textOutput("MTTF_Output"))
-      )
-    
-  )
-  
-))
->>>>>>> b55d5f4038b260fb12b52a13a4499ac4756f9d9f
+
 )
